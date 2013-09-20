9088bf2ab83de8432a32c806b17e5fd1328f1941
ryo-murai <murai.ry@gmail.com>
2013-09-20 11:41:45 +0900

    added alias to vim

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,7 @@
 # http://stackoverflow.com/questions/1275090/what-exactly-is-exit-in-powershell
 # exit with Ctrl+D<Enter>
 iex "function $([char]4) { exit }"
+
+# alias to vim in standard msys installation
+sal vim "$env:ProgramFiles\Git\share\vim\vim73\vim.exe"
+
