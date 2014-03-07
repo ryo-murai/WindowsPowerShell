0a10e815ef6c6a647c3ca985c0e93b5c9009d4e9
ryo-murai <murai.ry@gmail.com>
2014-03-07 14:20:06 +0900

    added md5sum

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -20,3 +20,15 @@ function psversion {
     echo "1.0"
   }
 }
+
+function md5sum {
+  param([string]$file)
+
+  $md5 = [System.Security.Cryptography.MD5]::Create()
+  $fstream = New-Object IO.StreamReader $file
+  $hash = $md5.ComputeHash($fstream.BaseStream)
+  $fstream.Close()
+  $hs = ""
+  $hash | % { $hs += $_.ToString("x2") }
+  $hs
+}
